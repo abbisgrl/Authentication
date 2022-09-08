24f4d1ca9d94361830eeac5e67a372e47786ef39
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-09-08 16:49:48 +0530

    MongoDb Connection

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+const db = require('./config/keys').MongoURI;
 
 const app = express();
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).
+    then(() => {
+        console.log('MongoDb connection is successful');
+    }).catch((err) => {
+        console.log(err);
+    })
+
 
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose =require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true    
+    }
+    ,email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    verified:{
+        type:Boolean,
+        default:false
+    },
+    resetLink:{
+        type:String,
+        default:false
+    }
+},{
+    timestamps:true
+});
+
+const User = mongoose.model('User',userSchema);
+export default User;
