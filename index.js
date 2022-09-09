7de8751d4d1f732b9f9bc620d48e8e42f8c8c136
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-09-09 22:35:22 +0530

    passport file

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const expressLayout = require('express-ejs-layouts');
-
+const passport = require('./config/passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const customFlash = require('./config/flash');
 
 const app = express();
 
+//passport configuration 
+require('./config/passport')(passport);
 const db = require('./config/keys').MongoURI;
 
 
+//mongodb connection
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).
     then(() => {
         console.log('MongoDb connection is successful');
@@ -16,16 +22,37 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).
     })
 
 
-
+//ejs configuration 
 app.use(expressLayout);
-app.use('/assets',express.static('./assets'));
-app.set('view engine','ejs');
+app.use('/assets', express.static('./assets'));
+app.set('view engine', 'ejs');
+
+//body parser configuration
+app.use(express.urlencoded({ extended: false }));
 
 
+//express session configuration
+app.use(
+    session({
+        secret:'secret',
+        resave:true,
+        saveUninitialized:true
+    })
+);
 
-app.use(express.urlencoded({extended:false}));
+
+//passport middleware 
+app.use(passport.initialize());
+app.use(passport.session());
 
 
+//connecting flash 
+app.use(flash());
+
+//global variables
+app.use(customFlash.setFlash);
+
+// Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
 
