78efe585c1a841d5a6883bd90b628371b3720396
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-09-10 12:32:04 +0530

    adding the auth js router file

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/user',(req,res)=>{
-    res.send("Gyanendra Shah");
+const authController = require('../controllers/authController');
+
+
+router.get('/login', (req, res) => res.render('login'));
+
+router.get('/forgot', (req, res) => res.render('forgot'));
+
+
+router.get('/reset/:id', (req, res) => {
+    res.render('reset', { id: req.params.id })
 });
 
-module.exports =router;
+router.get('/register', (req, res) => res.render('register'));
+
+router.post('/register', authController.registerHandle);
+
+router.get('/activate/:token', authController.activateHandle);
+
+router.post('/forgot', authController.forgotPassword);
+
+router.post('/reset/:id', authController.resetPassword);
+
+router.get('/forgot/:token', authController.gotoReset);
+
+router.post('/login', authController.loginHandle);
+
+router.get('/logout', authController.logoutHandle);
+
+module.exports = router;
