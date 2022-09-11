66b67d354c4a33bf1641a79f862f3d560d68b64a
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-09-11 15:34:37 +0530

    styling and assets

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 
-
+//------------ Local User Model ------------//
 const User = require('../models/User');
 
 module.exports = function (passport) {
